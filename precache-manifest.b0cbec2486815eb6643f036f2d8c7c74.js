e58fbc85dc4e1be665281f357610c771f2388374
Dinh DInh Truong <truondin@MacBookAir.local>
2023-02-02 00:32:28 +0100

    Updates

---
+++ precache-manifest.b0cbec2486815eb6643f036f2d8c7c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b728d44134c6489023b4",
+    "url": "/Thao-Martin_Wedding-Web/static/css/main.ec195939.chunk.css"
+  },
+  {
+    "revision": "b728d44134c6489023b4",
+    "url": "/Thao-Martin_Wedding-Web/static/js/main.b728d441.chunk.js"
+  },
+  {
+    "revision": "a5a369cade9a9a95f09e",
+    "url": "/Thao-Martin_Wedding-Web/static/js/1.a5a369ca.chunk.js"
+  },
+  {
+    "revision": "1a0d633cc986f8cc9489",
+    "url": "/Thao-Martin_Wedding-Web/static/js/2.1a0d633c.chunk.js"
+  },
+  {
+    "revision": "923a53c021cdd20b15f2",
+    "url": "/Thao-Martin_Wedding-Web/static/js/runtime~main.923a53c0.js"
+  },
+  {
+    "revision": "2ec217e9cde74b9a6d736076c3ef848f",
+    "url": "/Thao-Martin_Wedding-Web/index.html"
+  }
+];
