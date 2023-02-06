8055bf9a7d69fcf616c092490518d5971fe80134
truondin <dannenr@gmail.com>
2023-02-06 18:29:34 +0100

    Updates

---
+++ precache-manifest.e58825ecbc7426af73d810011e33da81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33ec000c2e6cc50a2d70",
+    "url": "/Thao-Martin_Wedding-Web/static/css/main.70661ee1.chunk.css"
+  },
+  {
+    "revision": "33ec000c2e6cc50a2d70",
+    "url": "/Thao-Martin_Wedding-Web/static/js/main.33ec000c.chunk.js"
+  },
+  {
+    "revision": "dbcaf9f90a7783afe4d3",
+    "url": "/Thao-Martin_Wedding-Web/static/js/1.dbcaf9f9.chunk.js"
+  },
+  {
+    "revision": "29d920ed61c6d2022976",
+    "url": "/Thao-Martin_Wedding-Web/static/js/2.29d920ed.chunk.js"
+  },
+  {
+    "revision": "c7db853a45434789a42a",
+    "url": "/Thao-Martin_Wedding-Web/static/js/runtime~main.c7db853a.js"
+  },
+  {
+    "revision": "86358c3ce96222ac8edd99a7e49196fb",
+    "url": "/Thao-Martin_Wedding-Web/index.html"
+  }
+];
