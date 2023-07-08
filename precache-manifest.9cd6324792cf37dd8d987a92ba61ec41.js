ea96fdf43a4ab9dfa4dc8080aba431ccd1106156
truondin <dannenr@gmail.com>
2023-07-08 15:15:59 +0200

    Updates

--- precache-manifest.9cd6324792cf37dd8d987a92ba61ec41.js
+++ precache-manifest.9cd6324792cf37dd8d987a92ba61ec41.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "882ed85344a36e24fa46",
+    "revision": "dd6ecaf63835b896e922",
     "url": "/Thao-Martin_Wedding-Web/static/css/main.70661ee1.chunk.css"
   },
   {
-    "revision": "882ed85344a36e24fa46",
-    "url": "/Thao-Martin_Wedding-Web/static/js/main.882ed853.chunk.js"
+    "revision": "dd6ecaf63835b896e922",
+    "url": "/Thao-Martin_Wedding-Web/static/js/main.dd6ecaf6.chunk.js"
   },
   {
     "revision": "dbcaf9f90a7783afe4d3",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/Thao-Martin_Wedding-Web/static/js/runtime~main.c7db853a.js"
   },
   {
-    "revision": "e528e5f8ff94798ff995ffd47064020f",
+    "revision": "e0e4e9cdaa4da4acfd5cc8a968a11784",
     "url": "/Thao-Martin_Wedding-Web/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Thao-Martin_Wedding-Web/precache-manifest.6cf7b92653e31617ee1b7ac6d94312ef.js"
+  "/Thao-Martin_Wedding-Web/precache-manifest.9cd6324792cf37dd8d987a92ba61ec41.js"
 );
 
 workbox.clientsClaim();
